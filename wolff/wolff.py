760b0fc58398f07d1fc611f5630ca460ca793924
Richard Wolff <richwolff12@gmail.com>
2022-09-30 15:32:59 -0400

    Update wolff.py
    Added download_sql_data function

--- wolff/wolff.py
+++ wolff/wolff.py
@@ -2,6 +2,10 @@
 import operator as oper
 from functools import reduce
 import pandas as pd
+import subprocess
+from pathlib import Path
+import tempfile
+import uuid
 
 
 operator_factory = {
@@ -53,3 +57,82 @@ def filter_pd(df, filters=None):
             reduce(oper.and_, conjunction_members)
         )  # equivalent to all()
     return df[reduce(oper.or_, disjunction_members)]  # equivalent to any()
+
+
+def download_sql_data(
+    server: str,
+    database: str,
+    query_file: Path,
+    output_file: Path = None,
+    auth_type: str = "sql_login",
+    uid: str = None,
+    pwd: str = None,
+    sep: str = "|",
+) -> subprocess.Popen.communicate:
+    """Download SQL Data From SQL Server using SQLCMD with AZURE AD auth.
+
+    Args:
+        server (str): Server path
+        database (str): Database Name
+        query_file (Path): Path to the file containing the query
+        output_file (Path): Output TSV to save. If None, a temporary output file will be generated. Defaults to None.
+        auth_type (str): Authentication Type. One of 'sql_login', 'windows_auth', 'add'. Defaults to 'sql_login'.
+        uid (str): Your User ID. Defaults to None.
+        pwd (str): Your Password. Defaults to None.
+        sep (str): Column Separator. Defaults to '|'.
+
+    Returns:
+        (output_file, subprocess.Popen.communicate): Tuple of output file and the Communication output from POPEN Process
+    """
+
+    auth_types = {"sql_login", "windows_auth", "add"}
+    if auth_type not in auth_types:
+        raise ValueError(
+            f"Argument auth_type must be one of ({auth_types}). You passed '{auth_type}'"
+        )
+
+    if auth_type in {"sql_login", "add"} and ((not uid) or (not pwd)):
+        raise ValueError(
+            "Username and Password required when using auth_type of '{auth_type}'."
+        )
+
+    # Generate output file if none
+    if output_file is None:
+        output_file = Path(tempfile.mkdtemp()) / f"{uuid.uuid1()}.csv"
+
+    output_path = output_file.parent
+    output_file_name = output_file.name
+    subprocess_args = [
+        "sqlcmd",
+        "-S",
+        f"{server}",
+        "-d",
+        f"{database}",
+        "-i",
+        f"{str(query_file)}",
+        "-o",
+        f"{output_file_name}",
+        "-W",  # REMOVE TRAILING SPACES
+        "-I",  # (enable quoted identifiers)
+        "-h",  # ROWS PER HEADER
+        "-1",  # ROWS PER HEADER
+        "-s",  # COL SEPARATOR
+        f"{sep}",  # COL SEPARATOR
+    ]
+
+    # Auth Arguments
+    if auth_type == "add":
+        subprocess_args.extend(["-G", "-U", f"{uid}", "-P", f"{pwd}"])
+    elif auth_type == "sql_login":
+        subprocess_args.extend(["-U", f"{uid}", "-P", f"{pwd}"])
+    elif auth_type == "windows_auth":
+        subprocess_args.extend(["-E"])
+
+    process = subprocess.Popen(
+        subprocess_args,
+        cwd=str(output_path),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    return (output_file, process.communicate())
